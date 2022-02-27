2adec3fc05b3860f0c6302cd8b0ac9953c75467f
Dansilllva <daniell-felipe10@hotmail.com>
2022-02-27 20:17:59 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,23 @@
 class Pessoa:
 
-    def __init__(self, nome=None, idade=27):
+    def __init__(self, *filhos, nome=None, idade=27):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Daniel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Felipe'
-    print(p.idade)
+    felipe = Pessoa(nome = 'Felipe')
+    daniel = Pessoa(felipe, nome='Daniel')
+    print(Pessoa.cumprimentar(daniel))
+    print(id(daniel))
+    print(daniel.cumprimentar())
+    print(daniel.nome)
+    print(daniel.idade)
+    for filho in daniel.filhos:
+        print(filho.nome)
+
+
+
